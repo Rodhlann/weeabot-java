ddb62c50e89ed5ad5d9eefdc55a2de0400db5611
rodhlann <tjpepper93@gmail.com>
2020-08-02 00:42:38 -0400

    rebase onto postgres branch

--- src/main/java/com/pepper/weeabot/DatabaseLoader.java
+++ src/main/java/com/pepper/weeabot/DatabaseLoader.java
@@ -1,5 +1,7 @@
 package com.pepper.weeabot;
 
+import com.pepper.weeabot.models.Anime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
